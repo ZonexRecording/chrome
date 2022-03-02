0edd015665306a2cc062bf3b79e8e536d21816de
ZonexRecording <96793036+ZonexRecording@users.noreply.github.com>
2022-03-02 04:25:42 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from webbot import Browser
+time.sleep(1)
+print("Installation complete!")
+print("Please wait...")
+time.sleep(3)
+print("Starting...")
+Chromium = Browser()
+print("Finished!")
+print("Chromium is now ready to use!!!!")
